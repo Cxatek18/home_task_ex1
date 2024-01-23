879fff9d1187552fe9fb2cfd1389924ba39c8f94
Cxatek18 <griga2888@mail.ru>
2024-01-23 17:55:59 +0500

    Implemented - moving the SeekBar behind the song, rewinding the song using the SeekBar.

--- app/src/main/java/com/team/musicex/MainActivity.kt
+++ app/src/main/java/com/team/musicex/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.team.musicex
 
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
 import com.team.musicex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +18,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         mediaPlayer = MediaPlayer.create(this, R.raw.cold_play_viva_la_diva)
+        initSeekBar()
         onClickButtonPlay()
+        trackingMovementSeekBar()
     }
 
     override fun onDestroy() {
@@ -27,11 +33,44 @@ class MainActivity : AppCompatActivity() {
             if (!mediaPlayer.isPlaying) {
                 mediaPlayer.start()
                 binding.btnPlay.setImageResource(R.drawable.pause_btn)
-                binding.seekBarMusic.progress = mediaPlayer.currentPosition
             } else {
                 mediaPlayer.pause()
                 binding.btnPlay.setImageResource(R.drawable.play_btn)
             }
         }
+    }
+
+    private fun initSeekBar() {
+        binding.seekBarMusic.max = mediaPlayer.duration
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                try {
+                    binding.seekBarMusic.progress = mediaPlayer.currentPosition
+                    handler.postDelayed(this, 1000)
+                } catch (e: Exception) {
+                    binding.seekBarMusic.progress = 0
+                }
+
+            }
+        }, 0)
+    }
+
+    private fun trackingMovementSeekBar() {
+        binding.seekBarMusic.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
     }
 }
